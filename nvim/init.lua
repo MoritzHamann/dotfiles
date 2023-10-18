57422099dd80646012bce97f75f0cef28b3efff3
Moritz <some.email@example.com>
2023-10-18 23:42:46 +0100

    adding color scheme and lsp-config

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,14 +18,33 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 
+
 -- setup plugins
 require("lazy").setup({
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" }
 	},
 	{
-		"github/copilot.vim"
+		"github/copilot.vim",
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").pyright.setup({})
+			require("lspconfig").tsserver.setup({})
+			require("lspconfig").gopls.setup({})
+		end
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
@@ -51,12 +70,17 @@ require("lazy").setup({
 	}
 })
 
+-- disable copilot on startup
+-- TODO
+
 -- setup editor
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.cmd.colorscheme "catppuccin-macchiato"
+-- vim.cmd.colorscheme "tokyonight-moon"
 
 
 -- setup keybindings
